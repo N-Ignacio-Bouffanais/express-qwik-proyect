60cb03bbd912dd520a773357a603a1f497d4e3c5
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-06-17 23:41:18 -0400

    [feature] services folder and some async functions

--- apps/server/src/controllers/users.ts
+++ apps/server/src/controllers/users.ts
@@ -1,4 +1,5 @@
 import { Response, Request } from "express";
+import { createUser, getUser, getUsers, deleteUser, updateUser } from "services/users";
 
 
 const welcome = async (req: Request, res: Response) => {
---
+++ apps/server/src/services/users.ts
@@ -0,0 +1,14 @@
+import { Response, Request } from "express";
+
+const createUser = async (req: Request, res: Response) => {};
+
+const getUsers = async (req: Request, res: Response) => {}
+
+const getUser = async (req: Request, res: Response) => {};
+
+const updateUser = async (req: Request, res: Response) => {};
+
+const deleteUser = async (req: Request, res: Response) => {};
+
+
+export { createUser, getUsers, getUser, updateUser, deleteUser };
